4d9c07bf30cedd172c232bd6bd0ae66a2ec15831
Maksud Nurullaev <maksud.nurullaev@gmail.com>
2021-03-07 04:29:15 +0500

    Add check DB functionality

--- lib/PERLogger.pm
+++ lib/PERLogger.pm
@@ -3,6 +3,7 @@ use Mojo::Base 'Mojolicious', -signatures;
 use Mojolicious::Routes;
 
 use Term::ANSIColor;
+use Data::Dumper;
 
 use Mojo::SQLite;
 use Mojo::Home;
@@ -13,48 +14,49 @@ sub startup ($self) {
     # Load configuration from config file
     my $config = $self->plugin('NotYAMLConfig');
 
-    my $home = Mojo::Home->new;
-
     # Configure the application
     $self->secrets( $config->{secrets} );
 
-    # Configure database
+    # Check configuration variables for database
     if ( !defined( $config->{dir4db} ) ) {
         _Prefix( "ERROR", "red",
 "Not defined location for SQLite database file (dir4db) in configuration file"
         );
         exit(1);
     }
-    else {
-        my $home = Mojo::Home->new;
-        $home->detect;
-        my $path2db = $home->rel_file( $config->{dir4db} );
 
-        if ( !-d $path2db->dirname ) {
-            _Prefix( "WARNING", "yellow",
-                "$path2db not exitst, trying to create new one..." );
-            make_path $path2db->dirname
-              or die "Failed to create path: " . $path2db->dirname;
+    # Check & Configure database
+    my $home = Mojo::Home->new;
+    $home->detect;
+    my $path2db = $home->rel_file( $config->{dir4db} );
+    my $dir     = $path2db->dirname();
+
+    # ... check database directory existance
+    if ( !-d $path2db->dirname ) {
+        _Prefix( "WARNING", "yellow",
+            "$path2db not exitst, trying to create new one..." );
+        make_path $dir
+          or die "$!: Failed to create path: " . $dir;
+    }
+
+    # ... create helper for sqlite
+    $self->helper(
+        sqlite => sub { state $sql = Mojo::SQLite->new( 'sqlite:' . $path2db ) }
+    );
+
+    # ... check for table existance
+    my $db = $self->sqlite->db;
+
+    if ( ! @{$db->tables} ) {
+        my @SQLITE_INIT_SQLs = (
+"CREATE TABLE logs (server varchar(64), logfile varchar(255), ltime TIMESTAMP DEFAULT CURRENT_TIMESTAMP, log TEXT );",
+"CREATE INDEX i_logs ON logs (server, logfile, ltime);",
+        );
+        for my $sql (@SQLITE_INIT_SQLs) {
+            my $sth = $db->dbh()->prepare($sql);
+            $sth->execute() || die "$!";
+            say $sql;
         }
-
-        $self->helper( sqlite =>
-              sub { state $sql = Mojo::SQLite->new( 'sqlite:' . $path2db ) } );
-
-        if ( !-e $path2db ) {
-            my @SQLITE_INIT_SQLs = (
-"CREATE TABLE IF NOT EXIST logs (server varchar(32), logfile varchar(255), ltime TIMESTAMP DEFAULT CURRENT_TIMESTAMP, log TEXT );",
-"CREATE INDEX IF NOT EXIST i_logs ON logs (server, logfile, ltime);",
-            );
-            for my $sql (@SQLITE_INIT_SQLs) {
-                my $stmt = $connection->prepare($sql);
-                $stmt->execute
-                  || die "Error:Db: Could not init database with: $sql";
-            }
-            $dbh->do('');
-            die "Failed to create db file, path: " . $path2db;
-        }
-
-        exit(1);
     }
 
     # Router
